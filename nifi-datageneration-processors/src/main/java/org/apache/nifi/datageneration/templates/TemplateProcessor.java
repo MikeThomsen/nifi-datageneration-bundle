2fe44ccbd4d725b1995d23842f1b79a5d551c085
Mike Thomsen <mikerthomsen@gmail.com>
2018-07-21 09:35:05 -0400

    Added skeleton of TemplateProcessor and added meta-inf config info.

---
+++ nifi-datageneration-processors/src/main/java/org/apache/nifi/datageneration/templates/TemplateProcessor.java
@@ -0,0 +1,12 @@
+package org.apache.nifi.datageneration.templates;
+
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.exception.ProcessException;
+
+public class TemplateProcessor extends AbstractProcessor {
+    public void onTrigger(ProcessContext processContext, ProcessSession processSession) throws ProcessException {
+        
+    }
+}
