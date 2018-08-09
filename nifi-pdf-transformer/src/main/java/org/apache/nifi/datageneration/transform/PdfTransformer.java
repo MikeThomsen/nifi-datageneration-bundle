e7952037d51052e7d93b6cc6d93f828ef49efb0f
Mike Thomsen <mikerthomsen@gmail.com>
2018-08-09 08:12:21 -0400

    Changed PdfTransformer to use PdfHandler.

--- nifi-pdf-transformer/src/main/java/org/apache/nifi/datageneration/transform/PdfTransformer.java
+++ nifi-pdf-transformer/src/main/java/org/apache/nifi/datageneration/transform/PdfTransformer.java
@@ -4,14 +4,55 @@ import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
+import org.apache.nifi.annotation.lifecycle.OnEnabled;
+import org.apache.nifi.components.AllowableValue;
+import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.controller.AbstractControllerService;
+import org.apache.nifi.controller.ConfigurationContext;
+import org.apache.nifi.processor.exception.ProcessException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PdfTransformer extends AbstractControllerService implements TransformationService {
     public static final String MIME_TYPE = "application/pdf";
+
+    static final AllowableValue RAW_TEXT = new AllowableValue("text", "Plain Text", "Converts each paragraph " +
+            "into a PDF paragraph and removes empty lines.");
+    static final PropertyDescriptor HANDLER = new PropertyDescriptor.Builder()
+        .name("handler")
+        .displayName("Handler")
+        .allowableValues(RAW_TEXT)
+        .defaultValue(RAW_TEXT.getValue())
+        .description("Choose a handler for parsing the output of the template processor.")
+        .required(true)
+        .build();
+
+    static final List<PropertyDescriptor> DESCRIPTORS;
+    static final Map<String, PdfHandler> HANDLERS;
+
+    static {
+        List<PropertyDescriptor> _temp = new ArrayList<>();
+        _temp.add(HANDLER);
+        DESCRIPTORS = Collections.unmodifiableList(_temp);
+
+        Map<String, PdfHandler> _h = new HashMap<>();
+        _h.put(RAW_TEXT.getValue(), new RawTextHandler());
+        HANDLERS = Collections.unmodifiableMap(_h);
+    }
+
+    private volatile PdfHandler pdfHandler;
+
+    @OnEnabled
+    public void onEnabled(ConfigurationContext context) {
+        String handler = context.getProperty(HANDLER).getValue();
+        pdfHandler = HANDLERS.get(handler);
+    }
 
     @Override
     public byte[] transform(byte[] input) {
@@ -22,27 +63,20 @@ public class PdfTransformer extends AbstractControllerService implements Transfo
     public byte[] transform(byte[] input, Map<String, String> attributes) {
         Document doc = new Document();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] retVal = null;
+        byte[] retVal;
 
         try {
             PdfWriter.getInstance(doc, out);
             doc.open();
 
-            String raw = new String(input);
-            String[] lines = raw.split("\n\r");
-            for (int x = 0; x < lines.length; x++) {
-                String clean = lines[x].trim();
-                if (!clean.isEmpty()) {
-                    doc.add(new Paragraph(clean));
-                }
-            }
+            pdfHandler.handle(doc, input, attributes);
 
             doc.close();
             out.close();
 
             retVal = out.toByteArray();
-        } catch (IOException | DocumentException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new ProcessException(e);
         }
         return retVal;
     }
