ad4dadc57d5524545a538dd624b92e4f6329a953
Mike Thomsen <mikerthomsen@gmail.com>
2018-08-28 14:33:26 -0400

    Wrote basic HTML -> DOCX transformer.

--- nifi-word-processing-transformer/src/main/java/org/apache/nifi/datageneration/transform/docx/DocxHtmlHandler.java
+++ nifi-word-processing-transformer/src/main/java/org/apache/nifi/datageneration/transform/docx/DocxHtmlHandler.java
@@ -1,13 +1,28 @@
 package org.apache.nifi.datageneration.transform.docx;
 
 import org.apache.nifi.datageneration.transform.WordProcessorHandler;
+import org.docx4j.convert.in.xhtml.XHTMLImporterImpl;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Map;
 
 public class DocxHtmlHandler implements WordProcessorHandler {
     @Override
     public byte[] transform(byte[] input, Map<String, String> attributes) throws Exception {
-        return new byte[0];
+        String content = new String(input);
+
+        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.createPackage();
+        XHTMLImporterImpl importer = new XHTMLImporterImpl(wordMLPackage);
+        importer.setHyperlinkStyle("Hyperlink");
+        wordMLPackage.getMainDocumentPart().getContent().addAll(
+                importer.convert(content, "/") );
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        wordMLPackage.save(out);
+        out.close();
+
+        return out.toByteArray();
     }
 
     @Override
