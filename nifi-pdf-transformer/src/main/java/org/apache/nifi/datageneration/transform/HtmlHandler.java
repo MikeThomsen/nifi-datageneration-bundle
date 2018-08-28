a4b140a0600f48a90ac451fa7986ced8e2702658
Mike Thomsen <mikerthomsen@gmail.com>
2018-08-28 14:14:20 -0400

    Removed cruft.

--- nifi-pdf-transformer/src/main/java/org/apache/nifi/datageneration/transform/HtmlHandler.java
+++ nifi-pdf-transformer/src/main/java/org/apache/nifi/datageneration/transform/HtmlHandler.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayOutputStream;
 import java.util.Map;
 
 public class HtmlHandler implements PdfHandler {
-    public static String CSS_SELECTOR_ATTRIBUTE = "css.selector";
     @Override
     public byte[] handle(byte[] input, Map<String, String> attributes) throws Exception {
         String content = new String(input);
